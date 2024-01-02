84d0272caf342edce6362088f099c929ef3e8516
Prommest Kavindechatorn <111680630+kavinddd@users.noreply.github.com>
2024-01-02 14:36:33 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,10 @@ module.exports = {
     mode: 'development',
     entry: {
         index: './src/index.js',
-        print: './src/print.js',
     },
     devtool: 'inline-source-map',
     devServer: {
-        static: '.dist',
+        static: './dist',
     },
     plugins: [
         new HtmlWebpackPlugin({
